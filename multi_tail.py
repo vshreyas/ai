6c979f4f4c865ce9bd9fb187e1cd2710cc098225
Shreyas Vinayakumar <shreyasvin@gmail.com>
2015-03-08 18:40:09 -0500

    Added optional arguments, fixed bug in output

--- multi_tail.py
+++ multi_tail.py
@@ -7,38 +7,42 @@ import os
 import sys
 from threading import Thread
 from queue import Queue
-
+from queue import Empty
 '''
 Implement multi-file tail
 '''
 
-# 
-# fn = 'test.log'
-# for sentence in watch(fn):
-#     print(json.dumps(sentence, sort_keys=True, indent=4,separators=(',',':')));
-
-interval = 1
+interval = 1.0
 
 def _fstat(filename):
     st_results = os.stat(filename)
     return (st_results[6], st_results[8])
 
 class LogFileReader(Thread):
-    def __init__(self, queue, fname, stats, seek_begin=False):
+    def __init__(self, queue, fname, stats, seek_begin=False, poll_time=10):
         Thread.__init__(self)
         self.que = queue
         self.filename = fname
         self.stats = stats
         self.seek_begin = seek_begin
+        self.poll_wait = poll_time
     
     def run(self):
+        if(self.seek_begin):
+            with open(self.filename, 'r') as fh:
+                while True:
+                    line = fh.readline()
+                    if not line:
+                        break
+                    process(line)
+            
         while 1:
         # print last_stats
             changed = False
             if self.check_file_for_changes():
                 changed = True
             if not changed:
-                time.sleep(interval)
+                time.sleep(self.poll_wait)
 
     def check_file_for_changes(self):
         changed = False
@@ -58,40 +62,65 @@ class LogFileReader(Thread):
                 line = fh.readline()
                 if not line:
                     break
-                try:
-                    msg = json.loads(line)
-                    if 'note' in msg and 'at' in msg:
-                        msg['input'] = self.filename
-                        self.que.put(msg)
-                    else:
-                        self.que.put(line)       
-                except ValueError:
-                    self.que.put(line)
+                self.process(line)
+                    
+    def process(self, line):
+        try:
+            msg = json.loads(line)
+            if 'note' in msg and 'at' in msg:
+                msg['input'] = self.filename
+                sentence = json.dumps(msg, sort_keys=True, indent=4,separators=(',',':'))
+                self.que.put(sentence)
+            else:
+                self.que.put('INVALID LINE:' + line)       
+        except ValueError:
+            self.que.put('INVALID LINE:' + line)
 
+        
 class OutputAppender(Thread):
-    def __init__(self, out = sys.stdout):
+    def __init__(self, out = sys.stdout, maxsize=2000):
         Thread.__init__(self)
         self.out = sys.stdout
+        self.max_size = maxsize
         self.queues = []
-    
+        self.output_queue = []
+
     def run(self):
         while True:
-            for queue in self.queues:
-                msg = queue.get()
-                sentence = json.dumps(msg, sort_keys=True, indent=4,separators=(',',':'))
-                queue.task_done()
-                self.out.write(sentence);
-            
+            while len(self.output_queue) < self.max_size:
+                for queue in self.queues:
+                    try:
+                        sentence = queue.get_nowait()
+                        self.output_queue.append(sentence) 
+                        queue.task_done()
+                    except Empty:
+                        pass
+                for queue in self.queues:
+                    try:
+                        sentence = queue.get(True, interval/(2 *len(self.queues)))
+                        self.output_queue.append(sentence) 
+                        queue.task_done()
+                    except Empty:
+                        pass
+                messages = sorted(self.output_queue)
+                for m in messages:
+                    print(m)
+                del self.output_queue
+                self.output_queue = []
 
-
-def multi_tail(root_dir, max_size=20000, interval=1):
-    S = lambda st_size_st_mtime: (max(0, st_size_st_mtime[0] - 124), st_size_st_mtime[1])
-    filenames = [ f for f in listdir(root_dir) if isfile(join(root_dir,f)) ]
-    last_stats = dict((fn, S(_fstat(fn))) for fn in filenames)
-    last_fn = None
-    last_print = 0  
+def multi_tail(root_dir, max_size=20000, interval=1.0):
     
-    writer = OutputAppender()
+    filenames = [ ]
+    for f in listdir(root_dir):
+         if isfile(join(root_dir,f)):
+             if f.split(".")[-1] == 'log':
+                 filenames.append(f)
+                 
+    last_stats = dict((fn, _fstat(fn)) for fn in filenames)
+    last_fn = None
+    last_print = 0
+    
+    writer = OutputAppender(max_size/(len(filenames) + 1))
     for fn in filenames:
         queue = Queue(max_size/(len(filenames) + 1))
         writer.queues.append(queue)
@@ -104,11 +133,11 @@ if '__main__' == __name__:
     op = OptionParser()
     op.add_option('-D', '--directory', help='/path/to/dir',
         type='str', default='.')
-    op.add_option('--interval', help='check interval, in seconds', type='int',
-        default=1)
+    op.add_option('-T', '--interval', help='check interval, in milliseconds', type='int',
+        default=1000)
     opts, args = op.parse_args()
     try:
-        multi_tail(root_dir=opts.directory, max_size=2000, interval=opts.interval)
+        multi_tail(root_dir=opts.directory, max_size=2000, interval=opts.interval/1000)
     except KeyboardInterrupt:
         pass
 
