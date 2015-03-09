bde39f28d8e67fb3308f1b64e985c51414d593f4
Shreyas Vinayakumar <shreyasvin@gmail.com>
2015-03-09 07:47:54 -0500

    Added design description to README

--- README.md
+++ README.md
@@ -1,2 +1,132 @@
+Programming Languages Used
+
+---------------------------------------------
+Python 3.2
+
+Key Assumptions
+
+---------------------------------------------
+
+1. Timestamps are in the format specified in the example, and use one of 30 well known timezone abbreviations
+
+2.  Last-modified date is updated correctly for each file
+
+3.  UTF-8 encoding is used
+
+
+Requirements
+
+---------------------------------------------
+
+Python installation
+python-dateutils
+
+
+Setup
+
+----------------------------------------------
+Install Python
+pip3 install dateutils
+
+Running the program
+
+----------------------------------------------
+<progname> -h gives help instructions
+<progname> [-D  /path/to/directory [–T xxx [-B]]]
+
+
+
+
+
+
+
+
+Design and architecture
+
+----------------------------------------------
+Multi-threading was used to promote concurrent reading of log files with fairness. Turnaround time  for each message may be slightly higher than the specified interval T. 
+
+High level description:
+
+Files are polled periodically to detect changes by a set of reader threads, which write to their individual buffers(FIFO queues). A writer thread assimilates the contents of these buffers in a round robin fashion, sorts them by the timestamp value and writes to output. Locking is used to control access to shared queues.
+
+Pseudo code:
+
+1.Program scans directory for log files and adds them to the list of files to monitor
+It collects initial stats for last modified date and file size for each of these files
+
+2.Main process then launches threads to monitor each file for changes. 
+
+Each LogReader thread keeps the following variables:
+
+- Last position read to in each file
+- Queue object shared with output thread
+- 
+
+and runs the following code:
+
+2.1.  Iterate and enqueue the last line(default) or all the lines(-B option) of the file that are available at the beginning.
+2.2  Scan the file for changes 
+2.3 If a text message has been appended, read it from the file, parse it to JSON with error handling, enqueue the augmented message in the queue for that file. If queue is full, 
+2.4  Sleep for a period of T/2*(n+1) to prevent stalling of CPU and OS
+2.5 Go back to step 2.2
+
+3. Main process also launches output thread in parallel. This thread runs the following procedure:
+
+3.1 Iterate over all input queues issuing a non-blocking call to 
+3.2 
+
+
+
+
+
+
+
+
+Testing Platform
+
+-----------------------------------------------
+
+Processor: Intel Core i3-2370 CPU @2.4GHz
+
+Installed RAM: 6GB
+
+Working memory at time of execution (limit set in environment): 1MB
+
+Operating System: Ubuntu 12.0.4(Linux kernel 3.2.0-29-generic)
+
+
+
+
+
+Design
+
+------------------------------------------------
+The basic blocks of the design consist of: (highlight and explain the buffer design)
+Common Functions :
+_fstat(filename)
+getKey(json_msg)
+
+Classname :
+ 
+LogFileReader 
+The purpose of this class is to perform line flush from the logfiles as and when additional  lines are added to the logfiles or when the logfile is regenerated.
+Functions
+__init__(self, queue, fname, stats, seek_begin, poll_time=10)
+run(self)
+check_file_for_changes(self)
+get_latest_message(self, pos)
+process(self, line)
+OutputAppender
+This class is intended to read lines from the buffer(s) and write out.
+Functions :
+__init__(self, out = sys.stdout, maxsize=2000)
+run(self)
+multi_tail(root_dir, max_size=20000, interval=1.0, seek_begin=False)
+
+
+
+
+
 ai
 ==
--- multi_tail.py
+++ multi_tail.py
@@ -8,20 +8,72 @@ import sys
 from threading import Thread
 from queue import Queue
 from queue import Empty
+import dateutil.parser as dp
+import cgi
+from _datetime import datetime
+from datetime import datetime
+import dateutil
+from dateutil.tz import *
 '''
 Implement multi-file tail
 '''
 
-interval = 1.0
+tz_str = '''-12 Y
+-11 X NUT SST
+-10 W CKT HAST HST TAHT TKT
+-9 V AKST GAMT GIT HADT HNY
+-8 U AKDT CIST HAY HNP PST PT
+-7 T HAP HNR MST PDT
+-6 S CST EAST GALT HAR HNC MDT
+-5 R CDT COT EASST ECT EST ET HAC HNE PET
+-4 Q AST BOT CLT COST EDT FKT GYT HAE HNA PYT
+-3 P ADT ART BRT CLST FKST GFT HAA PMST PYST SRT UYT WGT
+-2 O BRST FNT PMDT UYST WGST
+-1 N AZOT CVT EGT
+0 Z EGST GMT UTC WET WT
+1 A CET DFT WAT WEDT WEST
+2 B CAT CEDT CEST EET SAST WAST
+3 C EAT EEDT EEST IDT MSK
+4 D AMT AZT GET GST KUYT MSD MUT RET SAMT SCT
+5 E AMST AQTT AZST HMT MAWT MVT PKT TFT TJT TMT UZT YEKT
+6 F ALMT BIOT BTT IOT KGT NOVT OMST YEKST
+7 G CXT DAVT HOVT ICT KRAT NOVST OMSST THA WIB
+8 H ACT AWST BDT BNT CAST HKT IRKT KRAST MYT PHT SGT ULAT WITA WST
+9 I AWDT IRKST JST KST PWT TLT WDT WIT YAKT
+10 K AEST ChST PGT VLAT YAKST YAPT
+11 L AEDT LHDT MAGT NCT PONT SBT VLAST VUT
+12 M ANAST ANAT FJT GILT MAGST MHT NZST PETST PETT TVT WFT
+13 FJST NZDT
+11.5 NFT
+10.5 ACDT LHST
+9.5 ACST
+6.5 CCT MMT
+5.75 NPT
+5.5 SLT
+4.5 AFT IRDT
+3.5 IRST
+-2.5 HAT NDT
+-3.5 HNT NST NT
+-4.5 HLV VET
+-9.5 MART MIT'''
+
+tzd = {}
+for tz_descr in map(str.split, tz_str.split('\n')):
+    tz_offset = int(float(tz_descr[0]) * 3600)
+    for tz_code in tz_descr[1:]:
+        tzd[tz_code] = tz_offset
 
 def _fstat(filename):
     st_results = os.stat(filename)
     return (st_results[6], st_results[8])
 
+def parse_date(timestr):
+    return dp.parse(timestr, tzinfos=tzd)
+
 def getKey(json_msg):
     if 'at' in json_msg:
-        return json_msg['at']
-    return json_msg['timestamp']
+        return parse_date(json_msg['at'])
+    return parse_date(json_msg['timestamp'])
 
 class LogFileReader(Thread):
     def __init__(self, queue, fname, stats, seek_begin, poll_time=10):
@@ -40,6 +92,17 @@ class LogFileReader(Thread):
                     if not line:
                         break
                     self.process(line)
+        else:
+            with open(self.filename, 'rb') as fh:
+                offs = -100
+                while True:
+                    fh.seek(offs, 2)
+                    lines = fh.readlines()
+                    if len(lines)>1:
+                        last = lines[-1]
+                        break
+                    offs *= 2      # Read last line.
+                self.process(last.decode("utf-8"))
             
         while 1:
         # print last_stats
@@ -76,25 +139,27 @@ class LogFileReader(Thread):
                 msg['input'] = self.filename
                 self.que.put(msg)
             else:
-                self.que.put({'# INVALID LINE':cgi.escape(line), 'timestamp':time()})       
+                self.que.put({'# INVALID LINE':cgi.escape(line), 'timestamp': datetime.now().strftime("%b %d %Y %H:%M:%S")+ datetime.now(tzlocal()).tzname()})       
         except ValueError:
-            self.que.put({'# INVALID LINE':cgi.escape(line), 'timestamp':time()})
+            self.que.put({'# INVALID LINE':cgi.escape(line), 'timestamp': datetime.now().strftime("%b %d %Y %H:%M:%S") + datetime.now(tzlocal()).tzname()})
+            print(datetime.now().strftime("%b %d %Y %H:%M:%S %Z"))
 
         
 class OutputAppender(Thread):
-    def __init__(self, out = sys.stdout, maxsize=2000):
+    def __init__(self, out = sys.stdout, maxsize=2000, interval=1.0):
         Thread.__init__(self)
         self.out = sys.stdout
         self.max_size = maxsize
         self.queues = []
         self.output_queue = []
+        self.interval = interval
 
     def run(self):
         while True:
             while len(self.output_queue) < self.max_size:
                 for queue in self.queues:
                     try:
-                        msg = queue.get(True, interval/(2 *len(self.queues)))
+                        msg = queue.get(True, self.interval/(2 *len(self.queues)))
                         self.output_queue.append(msg) 
                         queue.task_done()
                     except Empty:
@@ -118,7 +183,7 @@ def multi_tail(root_dir, max_size=20000, interval=1.0, seek_begin=False):
     last_fn = None
     last_print = 0
     
-    writer = OutputAppender(max_size/(len(filenames) + 1))
+    writer = OutputAppender(max_size/(len(filenames) + 1), interval)
     for fn in filenames:
         queue = Queue(max_size/(len(filenames) + 1))
         writer.queues.append(queue)
